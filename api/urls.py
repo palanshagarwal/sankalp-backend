b9ccf15006d732df1d6802814cfd155617169f20
pygeek <palanshagarwal@gmail.com>
2015-10-19 03:41:00 +0530

    events

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
 
 from api import views
+
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
     url(r'^events/',views.EventList.as_view()),
